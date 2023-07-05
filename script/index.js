9b9362fc94b9e0004a14513ff8084e466745de46
FedericoCalvi93 <federicocalvi.93@gmail.com>
2023-07-05 16:56:53 +0200

    first commit

---
+++ script/index.js
@@ -0,0 +1,46 @@
+const getBooks= function () {
+  fetch(
+    'https://striveschool-api.herokuapp.com/books'
+  )
+    .then((res) => {
+      if (res.ok) {
+        return res.json()
+      } else {
+        if (res.status === 404) {
+          throw new Error('Not found')
+        } else {
+          throw new Error('Errore nella chiamata API')
+        }
+      }
+    })
+    .then((data) => {
+      console.log(data)
+      let spinnerContainer = document.getElementById('spinner-container')
+      spinnerContainer.classList.add('d-none')
+data.forEach(element => {
+  let col1=document.getElementById('col1')
+  col1.innerHTML=` <div class="card" style="width": 18rem;">
+  <img src=${element.img[4]} class="card-img-top" alt="...">
+  <div class="card-body>
+  <h5 class="card-title"> ${element.title[4]} </h5>
+  <p class="card-text"> ${element.price[4]} </p>
+  <a href="#" class="btn btn-primary">Acquista</a>
+  </div>
+</div>
+`
+cardBody.appendChild(card-title);
+cardBody.appendChild(card-text);
+card.appendChild(img)
+card.appendChild(cardBody)
+const cardContainer = document.getElementById('col1')
+cardContainer.appendChild(card);
+return card;
+
+});
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+getBooks()
