0142e67b60baec7d8417cdf9d73fbba2eecc345c
FedericoCalvi93 <federicocalvi.93@gmail.com>
2023-07-05 18:31:41 +0200

    Ultime modifiche

--- script/index.js
+++ script/index.js
@@ -17,7 +17,7 @@ const getBooks= function () {
       console.log(data)
       let spinnerContainer = document.getElementById('spinner-container')
       spinnerContainer.classList.add('d-none')
-data.forEach(element => {
+data.forEach((element) => {
   const col1=document.getElementById('col1')
   col1.innerHTML=` <div class="card" style="width": 18rem;">
   <img src=${element.img} class="card-img-top" alt="...">
@@ -30,14 +30,62 @@ data.forEach(element => {
 `
 const col2=document.getElementById('col2')
   col2.innerHTML=` <div class="card" style="width": 18rem;">
-  <img src=${element.img} class="card-img-top" alt="...">
+  <img src=${"https://images-na.ssl-images-amazon.com/images/I/91ku%2B0LppPL.jpg"} class="card-img-top" alt="...">
   <div class="card-body>
-  <h5 class="card-title"> ${element.title} </h5>
-  <p class="card-text"> ${element.price} </p>
+  <h5 class="card-title"> ${7.59} </h5>
+  <p class="card-text"> ${"Shards of Hope (Psy-Changeling Novel, A)"} </p>
   <a href="#" class="btn btn-primary">Acquista</a>
   </div>
 </div>
 `
+
+const col3=document.getElementById('col3')
+  col3.innerHTML=` <div class="card" style="width": 18rem;">
+  <img src=${"https://images-na.ssl-images-amazon.com/images/I/91J28bj3PYL.jpg"} class="card-img-top" alt="...">
+  <div class="card-body>
+  <h5 class="card-title"> ${26.09} </h5>
+  <p class="card-text"> ${
+    "Gameboard of the Gods"} </p>
+  <a href="#" class="btn btn-primary">Acquista</a>
+  </div>
+</div>
+`
+const col4=document.getElementById('col4')
+  col4.innerHTML=` <div class="card" style="width": 18rem;">
+  <img src=${"https://images-na.ssl-images-amazon.com/images/I/91eGxsWGH7L.jpg"} class="card-img-top" alt="...">
+  <div class="card-body>
+  <h5 class="card-title"> ${17.06} </h5>
+  <p class="card-text"> ${
+    "Silver Silence (Psy-Changeling Trinity)"} </p>
+  <a href="#" class="btn btn-primary">Acquista</a>
+  </div>
+</div>
+`
+const col5=document.getElementById('col5')
+  col5.innerHTML=` <div class="card" style="width": 18rem;">
+  <img src=${
+    "https://images-na.ssl-images-amazon.com/images/I/51vw%2Bn7xZCL.jpg"} class="card-img-top" alt="...">
+  <div class="card-body>
+  <h5 class="card-title"> ${9.60} </h5>
+  <p class="card-text"> ${
+"The Battle of Evernight - Special Edition: The Bitterbynde Book #3 (The Bitterbynde Trilogy)"} </p>
+  <a href="#" class="btn btn-primary">Acquista</a>
+  </div>
+</div>
+`
+const col6=document.getElementById('col6')
+  col6.innerHTML=` <div class="card" style="width": 18rem;">
+  <img src=${
+  "https://images-na.ssl-images-amazon.com/images/I/514%2BSppz8AL.jpg"} class="card-img-top" alt="...">
+  <div class="card-body>
+  <h5 class="card-title"> ${4.84} </h5>
+  <p class="card-text"> ${
+    "Nightchaser"} </p>
+  <a href="#" class="btn btn-primary">Acquista</a>
+  </div>
+</div>
+`
+
 cardBody.appendChild(card-title);
 cardBody.appendChild(card-text);
 card.appendChild(img)
